028ea250cb0f6433dea85e14b0ee70e116aa9f46
HappySingh7837 <46443936+HappySingh7837@users.noreply.github.com>
2020-10-07 18:32:30 +0530

    responsive done

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 import 'package:website_prac/widgets/centred_view.dart';
+import 'package:website_prac/widgets/nav_drawer/nav_drawer.dart';
 import 'package:website_prac/widgets/navigation_bar/nav_bar.dart';
 
 import 'home_content_desktop.dart';
@@ -9,7 +10,9 @@ import 'home_content_mobile.dart';
 class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return ResponsiveBuilder(
+      builder: (context,sizinginfo)=>Scaffold(
+        drawer: sizinginfo.isMobile?NavigationDrawer():null,
       backgroundColor: Colors.white,
       body: CentredView(
         child: Column(
@@ -24,6 +27,7 @@ class HomeView extends StatelessWidget {
           ],
         ),
       ),
+     )
     );
   }
 }
